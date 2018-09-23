65aa0ca9b483202185030b827fe9e54cad62532b
Amit Mishra <am290121@gmail.com>
2018-09-23 20:02:49 +0530

    Initial Commit

---
+++ src/main/java/com/dnu/jenkinspipelinesample/JenkinsPipelineSampleApplication.java
@@ -0,0 +1,12 @@
+package com.dnu.jenkinspipelinesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineSampleApplication.class, args);
+	}
+}
